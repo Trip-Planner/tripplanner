a3bbbabc4c0a31b54873174b7549d19875cdd123
jokelake <pysiaz@hotmail.com>
2017-10-31 23:05:16 +0700

    add components
    add feature floder and components

--- triplanner/src/app/app.module.ts
+++ triplanner/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateplanComponent } from './feature/createplan/createplan.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateplanComponent
   ],
   imports: [
     BrowserModule
---
+++ triplanner/src/app/feature/createplan/createplan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-createplan',
+  templateUrl: './createplan.component.html',
+  styleUrls: ['./createplan.component.css']
+})
+export class CreateplanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
