542a4167dc7b52c5e4278018df4ac326d7d1477e
jokelake <pysiaz@hotmail.com>
2017-11-01 01:10:20 +0700

    fixed
    debug

--- triplanner/src/app/app.module.ts
+++ triplanner/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CreateplanComponent } from './feature/createplan/createplan.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CreateplanComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
