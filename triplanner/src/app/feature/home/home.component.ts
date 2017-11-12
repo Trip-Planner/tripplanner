2b5de61dad72f5478caa9478bd7c1518fa5baced
Thanjee_PH <pha.thanjira@github.com>
2017-11-12 23:25:53 +0700

    Update menubar

--- triplanner/src/app/feature/home/home.component.ts
+++ triplanner/src/app/feature/home/home.component.ts
@@ -9,10 +9,14 @@ import {Router} from "@angular/router";
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  status:string;
   constructor(private router: Router,private _dataService:DataService) { }
 
   ngOnInit() {
+  }
+
+  canlink(temp:string){
+    this.router.navigate([temp]);
   }
 
   logout(){
